6e28b5a9e5a26364df1a5286f297bdbd8d876026
João Pedro <joaopmoferreira@gmail.com>
2019-11-04 13:20:40 -0300

    finishing fully conected network

--- src/fc.py
+++ src/fc.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import torch
+
+class RedeTotalmenteConectada(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.numero_classes = 10
+
+        self.layer_1 = torch.nn.Linear(784,100)
+        self.layer_2 = torch.nn.Linear(100,10)
+
+        self.activation = torch.nn.ReLU()
+    
+    def forward(self,x):
+        first_layer = self.activation(self.layer_1(x))
+        second_layer = self.activation(self.layer_2(first_layer))
+        return second_layer
